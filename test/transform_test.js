ad4261c93616e53059b7421f90a869171ce33e4f
bharding2 <b.harding@gmail.com>
2016-04-09 23:14:14 -0700

    finally got this goddamn async test working

--- test/transform_test.js
+++ test/transform_test.js
@@ -1,5 +1,23 @@
 // testing
-const expect = require('chai').expect
+const expect = require('chai').expect;
+const handleFile = require(__dirname + '/../lib/handleFile');
 
-describe('it should read a bitmap file');
-describe('it should write to a new bitmap file');
+describe('bitmap file to a buffer', () => {
+  var answers = {
+    file: __dirname + '/../img/pikachu.bmp',
+    transOption: 'greyscale'
+  };
+
+  it('should load a bitmap into a buffer', (done) => {
+    handleFile.run(answers, (data) => {
+      expect(Buffer.isBuffer(data)).to.eql(true);
+      done();
+    });
+  });
+});
+
+// describe('extract header data');
+
+// describe('transforms');
+
+// describe('write bitmap from buffer');
